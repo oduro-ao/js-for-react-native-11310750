9d1da6f1882773f85550505ebd2a69020026e7ca
oduro-ao <oadu-owusu@st.ug.edu.gh>
2024-05-19 18:11:28 +0000

    Update UserInfo.js

--- UserInfo.js
+++ UserInfo.js
@@ -1,8 +1,24 @@
-import  "./Manipulation.js";  // Import the default export
+// userInfo.js
+import formatArrayStrings from "./Manipulation.js";
 
-function createUserProfile2() {
-    return "Hello World";  // Return a string instead of logging directly
+function createUserProfiles(originalNames, processArray) {
+    const modifiedNames = formatArrayStrings(originalNames, processArray);
+
+    const userProfiles = originalNames.map((name, index) => {
+        return {
+            id: index + 1,
+            originalName: name,
+            modifiedName: modifiedNames[index]
+        };
+    });
+
+    return userProfiles;
 }
 
-// Log the results to the console
-console.log(createUserProfile2());
+const originalNames = ["kofi", "ama", "rodri", "kingsly", "rudiger"];
+const processArray = [1, 2, 3, 4, 5];  
+const userProfiles = createUserProfiles(originalNames, processArray);
+
+console.log(userProfiles);
+
+export { createUserProfiles };
