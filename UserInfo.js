e3d96fd21dfa7583d8e625e3883abf29b752636a
oduro-ao <oadu-owusu@st.ug.edu.gh>
2024-05-19 18:05:14 +0000

    Create UserInfo.js

---
+++ UserInfo.js
@@ -0,0 +1,8 @@
+import  "./Manipulation.js";  // Import the default export
+
+function createUserProfile2() {
+    return "Hello World";  // Return a string instead of logging directly
+}
+
+// Log the results to the console
+console.log(createUserProfile2());
